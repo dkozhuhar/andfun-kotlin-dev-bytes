75192379c0b5ea1a7b7fc68d4380e74f373839dd
Kozhukhar Dmitry <dna.coj@gmail.com>
2019-08-19 19:08:15 +0300

    14

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,27 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VideosRepository (private val database: VideosDatabase) {
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val videos = Network.devbytes.getPlaylist().await()
+            database.videoDao.InsertAll(*videos.asDatabaseModel())
+        }
+    }
+
+    //actual videos visible to UI
+    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()
+    }
+}
