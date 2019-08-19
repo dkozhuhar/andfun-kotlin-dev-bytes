47ce15530622b9f82dd5bc4128171b8d07f922e8
Dmitry Kozhukhar <dna.coj@gmail.com>
2019-08-20 00:56:40 +0300

    dependency conflict resolved

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -19,6 +19,7 @@ package com.example.android.devbyteviewer.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+
 import com.example.android.devbyteviewer.database.VideosDatabase
 import com.example.android.devbyteviewer.database.asDomainModel
 import com.example.android.devbyteviewer.domain.Video
